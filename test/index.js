428c7bda9c57720ac9953370198094a4c21b646b
sanu1020 <sanuuthaya02@gmail.com>
2023-07-04 13:53:46 +0530

    updated

--- test/index.js
+++ test/index.js
@@ -1,1 +1,11 @@
-console.log("Hello, World from TEST!");
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
