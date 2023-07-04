7016e0b0e53cac239298410439b0d1e741e8e209
sanu1020 <sanuuthaya02@gmail.com>
2023-07-05 00:03:56 +0530

    updated

--- test/index.js
+++ test/index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello, World ArgoEKS ofkik oma tik!');
+  res.send('Hello, World ArgoEKS!');
 });
 
 app.listen(port, () => {
