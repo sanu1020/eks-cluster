cefa48cbdd168482369e4bdb9248037e97f38fa6
sanu1020 <sanuuthaya02@gmail.com>
2023-07-04 12:21:25 +0530

    updated

--- signupfront/src/Home.jsx
+++ signupfront/src/Home.jsx
@@ -11,7 +11,7 @@ const Home = () => {
                             <h1 className="display-4 fw-bolder mb-4
                             text-center text-white">Hey There....</h1>
                             <p className="lead text-center fs-4 mb-5 text-white">
-                                Welcome To Home
+                                Welcome To Home World
                             </p>
                         </div>
                     </div>
