d630ce8b7279286f28023c71b4a90d947c27d225
sanu1020 <sanuuthaya02@gmail.com>
2023-07-06 13:28:53 +0530

    updated

--- main.tf
+++ main.tf
@@ -3,6 +3,6 @@ provider "aws" {
 }
 
 resource "aws_instance" "terraform_state_test" {
-  ami                     = "ami-06cd706b6bacee637"
+  ami                     = "ami-06b09bfacae1453cb"
   instance_type           = "t2.micro"
 }
