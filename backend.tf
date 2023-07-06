e664411d400e06c078655af43cbe510ceba84ef3
sanu1020 <sanuuthaya02@gmail.com>
2023-07-06 16:12:28 +0530

    updated

--- backend.tf
+++ backend.tf
@@ -1,8 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "ter-state-test"
-    key            = "terraform.tfstate"
+    key            = "terraform.tfstate?versionId=HvtrsBPTOUrIdNPE70h8iBblinKLDt2f"
     region         = "us-east-1"
-    version_id = "HvtrsBPTOUrIdNPE70h8iBblinKLDt2f"
   }
 }
