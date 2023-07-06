21a59b4bf0dde770266304a7c81bbd8dacdd35cb
sanu1020 <sanuuthaya02@gmail.com>
2023-07-07 00:36:06 +0530

    updated

--- devops/terraform/main.tf
+++ devops/terraform/main.tf
@@ -35,7 +35,7 @@ data "aws_availability_zones" "available" {}
 
 locals {
   name   = "skyu-eks-cluster"
-  region = "us-east-2"
+  region = "us-east-1"
 
   vpc_cidr = "10.0.0.0/16"
   azs      = slice(data.aws_availability_zones.available.names, 0, 3)
