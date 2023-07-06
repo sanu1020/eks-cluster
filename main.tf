949e818e1574b7a05d97b496cf511706fef4eb37
sanu1020 <sanuuthaya02@gmail.com>
2023-07-06 13:20:55 +0530

    updated

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 provider "aws" {
-  region     = "eu-central-1"
+  region     = "us-east-1"
 }
 
 resource "aws_instance" "terraform_state_test" {
   ami                     = "ami-06cd706b6bacee637"
-  instance_type           = "m5.large"
+  instance_type           = "t2.micro"
 }
