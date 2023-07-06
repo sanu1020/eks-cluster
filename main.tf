6e56f0d965f1379d10ec560777d5c326be23508a
sanu1020 <sanuuthaya02@gmail.com>
2023-07-06 16:44:29 +0530

    updated

---
+++ main.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+  region = "us-east-1"   # Update with your desired region
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = "sanu-tes-teraform-state-bucket"   # Update with your desired bucket name
+
+  # Optional bucket configurations
+  acl    = "private"          # Update with your desired ACL (private, public-read, etc.)
+}
